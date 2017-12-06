ab2abeb8e6a0beea794fe86b114d9ffcd751f05c
Pierre-Hugues Husson <phh@archos.com>
2017-12-06 17:41:40 +0100

    Initial commit

---
+++ vndk.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+	vendor/vndk/lib64/libwifi-service.so:system/lib64/vndk/libwifi-service.so \
+	vendor/vndk/lib64/libwifi-system.so:system/lib64/vndk/libwifi-system.so \
+	vendor/vndk/lib/libmediacodecservice.so:system/lib/vndk/libmediacodecservice.so \
+	vendor/vndk/lib64/libkeymaster1.so:system/lib64/vndk/libkeymaster1.so \
+	vendor/vndk/lib64/libkeymaster_messages.so:system/lib64/vndk/libkeymaster_messages.so
