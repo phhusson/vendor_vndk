b440c950ff4227915d98ee928937f2e6bb5699a4
Pierre-Hugues Husson <phh@phh.me>
2017-12-09 22:41:00 +0100

    Include libavservices_minijail from AOSP 8.0 in vndk

--- vndk.mk
+++ vndk.mk
@@ -3,4 +3,7 @@ PRODUCT_COPY_FILES += \
 	vendor/vndk/lib64/libwifi-system.so:system/lib64/vndk/libwifi-system.so \
 	vendor/vndk/lib/libmediacodecservice.so:system/lib/vndk/libmediacodecservice.so \
 	vendor/vndk/lib64/libkeymaster1.so:system/lib64/vndk/libkeymaster1.so \
-	vendor/vndk/lib64/libkeymaster_messages.so:system/lib64/vndk/libkeymaster_messages.so
+	vendor/vndk/lib64/libkeymaster_messages.so:system/lib64/vndk/libkeymaster_messages.so \
+	vendor/vndk/lib64/libavservices_minijail.so:system/lib64/vndk/libavservices_minijail.so \
+	vendor/vndk/lib/libavservices_minijail.so:system/lib/vndk/libavservices_minijail.so \
+
